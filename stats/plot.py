5947d8762713be2012e4c69e1dd07948f1954f70
inesmarca <imarcarian@itba.edu.ar>
2022-03-18 16:43:30 -0300

    changes in plot

--- stats/plot.py
+++ stats/plot.py
@@ -2,17 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.collections import EllipseCollection
 import pandas as pd
+import math 
 
 df = pd.read_csv('../output.csv', sep=',')
 
 colored_neighbours = list()
 x = df['x']
 y = df['y']
-size = df['r']**2
+size = math.pi*df['r'] + 10
 r = df['r']
 neighbours = df['neighbours']
 cmap = plt.cm.hsv
-print(colored_neighbours)
 
 fig, ax = plt.subplots()
 sc = ax.scatter(x, y, s=size, cmap=cmap, picker=True)
@@ -32,20 +32,21 @@ def update_annot(ind):
     index = int(ind["ind"])
     pos = sc.get_offsets()[ind["ind"][0]]
     annot.xy = pos
-    text = "id: {}\nr: {}\nneighbours: {}".format(index, r[index], neighbours[index])
+    text = "id: {}\nr: {}\nneighbours: {}\nx: {}\ny: {}".format(index, r[index], neighbours[index], x[index], y[index])
     annot.color = 'r'
     annot.set_text(text)
     ax.scatter(pos[0], pos[1], color='r', s=size[index])
     # if len(colored_neighbours) > 0:
     for i in colored_neighbours:
-        ax.scatter(x[int(i)], y[int(i)], color='b', s=size[int(i)])
+        if i != index:
+            ax.scatter(x[int(i)], y[int(i)], color='b', s=size[int(i)])
 
     colored_neighbours.clear()
     colored_neighbours.append(index)
 
     neighbours_list = neighbours[index].replace('[', '').replace(']', '').split(',')
     for i in neighbours_list:
-        ax.scatter(x[int(i)], y[int(i)], color='g', s=size[int(i)])
+        ax.scatter(x[int(i)], y[int(i)], color='r', s=size[int(i)])
         colored_neighbours.append(i)
     
 
