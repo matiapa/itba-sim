5253659b6e51f595492cd5f1731fb5af5d4ade1e
inesmarca <imarcarian@itba.edu.ar>
2022-04-03 23:31:25 -0300

    added colors to gifs

--- TP2/animate.py
+++ TP2/animate.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import animation
-from matplotlib.colors import ListedColormap
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
 from dotenv import load_dotenv
 from tqdm import tqdm
 
@@ -18,24 +18,25 @@ matplotlib.rcParams['animation.ffmpeg_path'] = os.getenv('ffmpeg_path')
 def create_grid_2D(size, iteration):
     grid = np.zeros((size, size))
 
+    max_distance = np.sqrt((size/2)**2 + (size/2)**2)
+
     rows = df.loc[df['t'] == iteration].values
     for row in rows:
-        grid[int(row[1]), int(row[2])] = int(row[3])
+        grid[int(row[1]), int(row[2])] = np.sqrt((int(row[1]) - size/2)**2 + (int(row[2]) - size/2)**2) / max_distance
 
     return grid
 
 def animate_step_2D(frame):
-    global grid_size, img_plot, load_iter
+    global grid_size, img_plot, load_iter, ax
 
     if frame > 0:
-        new_grid = create_grid_2D(grid_size, frame)    
+        new_grid = create_grid_2D(grid_size, frame) 
         img_plot.set_data(new_grid)
 
     try:
         load_iter.__next__()
     except:
         pass
-    # plt.savefig(f'out/{frame}.png')
 
 def plot_2D(size, animate=False):
     global grid_size, img_plot
@@ -47,16 +48,17 @@ def plot_2D(size, animate=False):
 
     ax.set_xticks(np.arange(0, grid_size, 1), minor=True)
     ax.set_yticks(np.arange(0, grid_size, 1), minor=True)
-    # ax.grid(which='minor', color='grey', linestyle='-', linewidth=2)
 
     plt.tight_layout()
 
     grid_size = size
     grid = create_grid_2D(size, 0)
-    img_plot = ax.imshow(grid, interpolation='nearest', cmap=ListedColormap(['black', 'white']))
+    cmap = LinearSegmentedColormap.from_list('my_colormap', [(0, '#ffffff'),(0.0000000001, '#ff0000'), (0.4, '#ff9900'), (0.7, '#ffe135'), (1, '#00cd00')])   
+    img_plot = ax.imshow(grid, interpolation='nearest', cmap=cmap)
 
     if animate:
-        anim = animation.FuncAnimation(fig, frames=end_t, func=animate_step_2D, interval=500)
+        anim = animation.FuncAnimation(fig, frames=end_t, func=animate_step_2D, interval=300)
+        # anim.save('animation_2D.gif')
         video_writer = animation.FFMpegWriter(fps=2)
         anim.save('out/animation_2D.mp4', writer=video_writer)
     else:
@@ -66,33 +68,44 @@ def plot_2D(size, animate=False):
 
 def create_grid_3D(size, iteration):
     grid = np.zeros((size, size, size))
+    colors = np.zeros((size, size, size))
+
+    max_distance = np.sqrt((size/2)**2 + (size/2)**2 + (size/2)**2)
 
     rows = df.loc[df['t'] == iteration].values
     for row in rows:
-        grid[int(row[1]), int(row[2]), int(row[3])] = int(row[4])
+        grid[int(row[1]), int(row[2]), int(row[3])] = np.sqrt((int(row[1]) - size/2)**2 + (int(row[2]) - size/2)**2 + (int(row[3]) - size/2)**2) / max_distance
+        colors[int(row[1]), int(row[2]), int(row[3])] = np.sqrt((int(row[1]) - size/2)**2 + (int(row[2]) - size/2)**2 + (int(row[3]) - size/2)**2) / max_distance
 
-    return grid
+
+    return grid, colors
 
 def animate_step_3D(frame):
     global grid_size, ax, load_iter
 
     if frame > 0:
-        filled = create_grid_3D(grid_size, frame)
+        filled, colors = create_grid_3D(grid_size, frame)
 
         ax.clear()
         ax.set_xlabel("x")
         ax.set_ylabel("y")
         ax.set_zlabel("z")
-        ax.voxels(filled, edgecolors='gray', shade=False)
+        ax.voxels(filled, edgecolors='gray', shade=False, facecolors=plt.cm.viridis(colors))
 
     try:
         load_iter.__next__()
     except:
         pass
-    # plt.savefig(f'out/{frame}.png')
 
 def plot_3D(size, animate=False):
     global grid_size, img_plot, ax
+
+    distances_to_center = list()
+    colors = list()
+    voxelarray = None
+    first_time = True
+
+    max_distance = np.sqrt((grid_size/2)**2 + (grid_size/2)**2 + (grid_size/2)**2)
 
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
@@ -105,12 +118,12 @@ def plot_3D(size, animate=False):
     plt.tight_layout()
 
     grid_size = size
-    filled = create_grid_3D(size, 0)
-    img_plot = ax.voxels(filled, edgecolors='gray', shade=False)
+    filled, colors = create_grid_3D(size, 0)
+    img_plot = ax.voxels(filled, edgecolors='gray', shade=False, facecolors=plt.cm.viridis(colors))
 
     if animate:
-        # anim.save('out/animation_3D.gif')
-        anim = animation.FuncAnimation(fig, frames=end_t, func=animate_step_3D, interval=500)
+        anim = animation.FuncAnimation(fig, frames=end_t, func=animate_step_3D, interval=300)
+        # anim.save('animation_3D.gif')
         video_writer = animation.FFMpegWriter(fps=2)
         anim.save('out/animation_3D.mp4', writer=video_writer)
     else:
