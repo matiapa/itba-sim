0373a0e2c13ac1fe2130021de5037ba2d6ec8a15
Matias J. Apablaza <mapablaza@itba.edu.ar>
2022-05-09 12:15:52 -0300

    Grafico de energia listo

--- TP4/ej2.py
+++ TP4/ej2.py
@@ -7,8 +7,8 @@ import numpy as np
 # ---------------------------------------------------------------
 
 # Parametros de la simulacion
-dt = 1e-13
-tf = 1e3*dt
+dt = 1e-14
+tf = 1.5e-12
 log_step = 1
 
 # Parametros del sistema
@@ -20,8 +20,12 @@ k = 1e10
 L = (sqrt(N)-1)*D
 
 # Condiciones iniciales
-v0 = np.array([10e3, 0])
-r0 = np.array([0, L/2])
+v0 = np.array([10e4, 0])
+r0 = np.array([0, L/2 + D/3])
+
+# ---------------------------------------------------------------
+#                         SIMULACION
+# ---------------------------------------------------------------
 
 # Expresion de la fuerza
 def f(ri, v):
@@ -54,8 +58,17 @@ def potential_energy(r):
 def should_stop(r, step):
     if step == 0:
         return False
-    if step > tf/dt:
+
+    if step*dt > tf:
         return True
+    
+    # Check if particle is inside valid coordinates
+
+    r_t = r[step]
+    if r_t[0]<0 or r_t[0]>L or r_t[1]<0 or r_t[1]>L:
+        return True
+
+    # Check if particle left the grid after entering
 
     r_t = r[step-1]
     was_inside_grid = r_t[0]>D and r_t[0]<L+D and r_t[1]>0 and r_t[1]<L
@@ -64,9 +77,7 @@ def should_stop(r, step):
     is_inside_grid = r_t[0]>D and r_t[0]<L+D and r_t[1]>0 and r_t[1]<L
 
     if was_inside_grid and not is_inside_grid:
-        print('Cut condition activated')
-
-    return was_inside_grid and not is_inside_grid
+        return True
 
 
 def verlett():
@@ -84,22 +95,32 @@ def verlett():
     step = 1
     t = step*dt
 
-    while not should_stop(r, step):
-        if step % log_step == 0:
-            print(f'{step} - R: {r[step]} - V: {v[step]} - F: {f(r[step], v[step])}')
+    try:
+        while not should_stop(r, step):
+            # if step % log_step == 0:
+                # print(f'{step} - R: {r[step]} - V: {v[step]} - F: {f(r[step], v[step])}')
+                # print(f'{step} - R: {r[step]} - V: {round(np.linalg.norm(v[step]), 2)} - A: {round(np.linalg.norm(f(r[step], v[step]))/M, 2)}')
 
-        # Obtenemos la próxima posición
-        r.append( 2 * r[step] - r[step-1] + dt**2 * f(r[step], v[step])/M )
+            # Obtenemos la próxima posición
+            r.append( 2 * r[step] - r[step-1] + dt**2 * f(r[step], v[step])/M )
 
-        # Obtenemos la próxima velocidad
-        v.append( (r[step+1]-r[step-1])/(2*dt) )
+            if step % log_step == 0:
+                print(f'{step} - R: {r[step]} - V: {round(np.linalg.norm(v[step]), 2)} - DR: {np.linalg.norm(r[-1]-r[-2])}')
 
-        # Avanzamos el tiempo de la simulacion
-        step += 1
-        t = step*dt
+            # Obtenemos la próxima velocidad
+            v.append( (r[step+1]-r[step-1])/(2*dt) )
+
+            # Avanzamos el tiempo de la simulacion
+            step += 1
+            t = step*dt
+    except Exception:
+        pass
 
     return r,v
 
+# ---------------------------------------------------------------
+#                         ANALISIS
+# ---------------------------------------------------------------
 
 def energy_check():
     rs, vs = verlett()
@@ -112,11 +133,38 @@ def energy_check():
         maxdiff = max(maxdiff, abs(up[-1]+uk[-1] - (up[0]+uk[0])))
     print(f'Max energy variation: {maxdiff/(up[0]+uk[0])*100}%')
     
-    times = np.arange(0, len(rs), 1) * dt
-    # pyplot.plot(times, up, label='Potential')
-    # pyplot.plot(times, uk, label='Kinetic')
-    pyplot.plot(times, np.array(up)+np.array(uk), label='Total')
+    times = np.arange(0, len(rs), 1)
+    pyplot.plot(times, up, label='Potential')
+    pyplot.plot(times, uk, label='Kinetic')
 
+    pyplot.legend()
+    pyplot.show()
+
+
+def energy_plot():
+    global r0, dt
+
+    for _dt in [1e-13, 1e-14, 1e-15]:
+        u_dt = []
+        # for y in [L/2]:
+        for y in np.linspace(L/2-D, L/2+D, 5, True):
+            r0 = np.array([0, y])
+            dt = _dt
+
+            print(f'Running dt={dt}, y={y}')
+            rs, vs = verlett()
+
+            u0 = potential_energy(rs[0]) + 0.5 * M * (vs[0][0]**2+vs[0][1]**2)
+
+            for i in range(len(rs)):
+                r, v = rs[i], vs[i]
+                if len(u_dt)<=i:
+                    u_dt.append(0)
+                u_dt[i] += abs(potential_energy(r) + 0.5 * M * (v[0]**2+v[1]**2) - u0) * 100 / (u0 * len(rs))
+        
+        times = np.arange(0, len(rs), 1)*dt
+        pyplot.plot(times, u_dt, label=f'dt={dt}')
+    
     pyplot.yscale('log')
     pyplot.legend()
     pyplot.show()
@@ -134,7 +182,6 @@ def plot_force():
                 force /= np.linalg.norm(force)
                 fx.append(force[0])
                 fy.append(force[1])
-        # print(x,y,force[0],force[1])
     
     px, py, c = [], [], []
     qsign = 1
@@ -173,11 +220,11 @@ def animate():
     scat = pyplot.scatter([rs[0][0]], [rs[0][1]], c=['g'])
 
     ani = animation.FuncAnimation(fig, update_plot, frames=range(len(rs)), fargs=(rs, scat))
-    pyplot.show()
+    # pyplot.show()
 
-    ani.save('out/animation.gif')
+    ani.save('out/animation.gif', fps=4*(1e-13/dt))
 
 
-# plot_force()
 # energy_check()
-animate()
+# animate()
+# energy_plot()
