2d2a5b0f0b5b541f8c1a3b7a3ad6b91cc48555de
Matias J. Apablaza <mapablaza@itba.edu.ar>
2022-05-12 15:53:40 -0300

    Clean up

--- TP4/ej2.py
+++ TP4/ej2.py
@@ -1,4 +1,4 @@
-from math import ceil, exp, cos, hypot, sqrt
+from math import hypot, sqrt
 from matplotlib import animation, pyplot
 import numpy as np
 import math
@@ -8,23 +8,14 @@ import random
 #                   CONDICIONES DEL PROBLEMA
 # ---------------------------------------------------------------
 
-# Parametros de la simulacion
-dt = 1e-13
-# tf = 2e-12
-tf = np.Infinity
-log_step = 1
-
-# Parametros del sistema
+# Parámetros físicos
 N = 16**2
 D = 1e-8
 M = 1e-27
 Q = 1e-19
 k = 1e10
 L = (sqrt(N)-1)*D
-
-# Condiciones iniciales
-v0 = np.array([5e3, 0])
-r0 = np.array([0, L/3])
+absortion_distance = 0.01*D
 
 stop_reason = ''
 
@@ -32,24 +23,22 @@ stop_reason = ''
 #                         SIMULACION
 # ---------------------------------------------------------------
 
-# Expresion de la fuerza
 def f(ri, v):
     qsign = 1
-    force = np.array([0.0,0.0])
+    force = np.array([0.0, 0.0])
     for px in np.linspace(D, L+D, int(sqrt(N)), endpoint=True):
         for py in np.linspace(0, L, int(sqrt(N)), endpoint=True):
             rij = ri - [px, py]
             rij_norm = np.linalg.norm(rij)
 
-            if rij_norm < 0.01*D:
-                raise Exception('Particle absorbed')
+            if rij_norm < absortion_distance:
+                return None
 
             force += k * Q**2 * (qsign / rij_norm**2) * rij/rij_norm
             qsign *= -1
         qsign *= -1
     return force
 
-# Expresion de la energia potencial
 def potential_energy(r):
     qsign, u = 1, 0
     for px in np.linspace(D, L+D, int(sqrt(N)), endpoint=True):
@@ -62,12 +51,13 @@ def potential_energy(r):
 
 def should_stop(r, step):
     global stop_reason
+
     if step == 0:
         return False
 
-    # if step*dt > tf:
-    #     stop_reason = 'timeout'
-    #     return True
+    if step*dt > tf:
+        stop_reason = 'timeout'
+        return True
     
     # Check if particle is inside valid coordinates
 
@@ -98,12 +88,12 @@ def should_stop(r, step):
         return True
 
 
-def verlett(v0, r0, animate=False):
-    global stop_reason, absorbed, exited, left, right, top, bottom
+def verlett(r0, v0, ovito=False):
+    global stop_reason
 
     # Inicializamos el problema
-    r = [r0]
-    v = [v0]
+    r = [np.array(r0)]
+    v = [np.array(v0)]
     step = 0
 
     # print(f'{step} - R: {r[step]} - V: {v[step]} - F: {f(r[step], v[step])}')
@@ -115,29 +105,27 @@ def verlett(v0, r0, animate=False):
     step = 1
     t = step*dt
 
-    try:
-        while not should_stop(r, step):
-            # if step % log_step == 0:
-                # print(f'{step} - R: {r[step]} - V: {v[step]} - F: {f(r[step], v[step])}')
-                # print(f'{step} - R: {r[step]} - V: {round(np.linalg.norm(v[step]), 2)} - A: {round(np.linalg.norm(f(r[step], v[step]))/M, 2)}')
+    while not should_stop(r, step):
+        force = f(r[step], v[step])
+        if force is None:
+            stop_reason = 'particle_absorbed'
+            break
+        
+        if step % log_step == 0:
+            print(f'{step} - R: {r[step]} - V: {v[step]} - F: {force}')
+            # print(f'{step} - R: {r[step]} - V: {round(np.linalg.norm(v[step]), 2)} - A: {round(np.linalg.norm(f(r[step], v[step]))/M, 2)}')
 
-            # Obtenemos la próxima posición
-            r.append( 2 * r[step] - r[step-1] + dt**2 * f(r[step], v[step])/M )
+        # Obtenemos la próxima posición
+        r.append( 2 * r[step] - r[step-1] + dt**2 * force/M )
 
-            # if step % log_step == 0:
-            #     print(f'{step} - R: {r[step]} - V: {round(np.linalg.norm(v[step]), 2)} - DR: {np.linalg.norm(r[-1]-r[-2])}')
+        # Obtenemos la próxima velocidad
+        v.append( (r[step+1]-r[step-1])/(2*dt) )
 
-            # Obtenemos la próxima velocidad
-            v.append( (r[step+1]-r[step-1])/(2*dt) )
+        # Avanzamos el tiempo de la simulacion
+        step += 1
+        t = step*dt
 
-            # Avanzamos el tiempo de la simulacion
-            step += 1
-            t = step*dt
-    except Exception:
-        stop_reason = 'particle_absorbed'
-        pass
-
-    if animate:
+    if ovito:
         with open('verlett.xyz', 'w') as file:
             for i in range(len(r)):
                 file.write(f'{N+4+1}\n\n')
@@ -154,31 +142,12 @@ def verlett(v0, r0, animate=False):
                 
                 file.write('{} {} {} 2e-9 0 1 0\n'.format(N+1, r[i][0], r[i][1]))
 
-    print(stop_reason)
+    # print(stop_reason)
     return r,v
 
 # ---------------------------------------------------------------
 #                         ANALISIS
 # ---------------------------------------------------------------
-
-def energy_check():
-    rs, vs = verlett(v0, r0)
-
-    up, uk, maxdiff = [], [], 0
-    for i in range(len(rs)):
-        r, v = rs[i], vs[i]
-        up.append( potential_energy(r) )
-        uk.append( 0.5 * M * (v[0]**2+v[1]**2) )
-        maxdiff = max(maxdiff, abs(up[-1]+uk[-1] - (up[0]+uk[0])))
-    print(f'Max energy variation: {maxdiff/(up[0]+uk[0])*100}%')
-    
-    times = np.arange(0, len(rs), 1)
-    pyplot.plot(times, up, label='Potential')
-    pyplot.plot(times, uk, label='Kinetic')
-
-    pyplot.legend()
-    pyplot.show()
-
 
 def plot_force():
     ex, ey, fx, fy = [], [], [], []
@@ -213,7 +182,7 @@ def update_plot(i, rs, scat):
     return scat
 
 
-def animate():
+def animate(r0, v0):
     rs, vs = verlett(v0, r0)
 
     x, y, c = [], [], []
@@ -236,22 +205,40 @@ def animate():
     ani.save('out/animation.gif', fps=8*(1e-13/dt))
 
 
+def energy_check(r0, v0):
+    rs, vs = verlett(v0, r0)
+
+    up, uk, maxdiff = [], [], 0
+    for i in range(len(rs)):
+        r, v = rs[i], vs[i]
+        up.append( potential_energy(r) )
+        uk.append( 0.5 * M * (v[0]**2+v[1]**2) )
+        maxdiff = max(maxdiff, abs(up[-1]+uk[-1] - (up[0]+uk[0])))
+    print(f'Max energy variation: {maxdiff/(up[0]+uk[0])*100}%')
+    
+    times = np.arange(0, len(rs), 1)
+    pyplot.plot(times, up, label='Potential')
+    pyplot.plot(times, uk, label='Kinetic')
+
+    pyplot.legend()
+    pyplot.show()
+
+
 # ---------------------------------------------------------------
 #                         GRAFICOS
 # ---------------------------------------------------------------
 
-def energy_plot():
-    global r0, dt
+def energy_plot(v0):
+    global  dt
 
     for _dt in [0.5e-12, 1e-13, 1e-14, 1e-15]:
         u_dt = []
         for y in [L/2]:
         # for y in np.linspace(L/2-D, L/2+D, 5, True):
-            r0 = np.array([0, y])
             dt = _dt
 
             print(f'Running dt={dt}, y={y}')
-            rs, vs = verlett(v0, r0)
+            rs, vs = verlett(r0=[0, y], v0=v0)
 
             u0 = potential_energy(rs[0]) + 0.5 * M * (vs[0][0]**2+vs[0][1]**2)
 
@@ -262,7 +249,7 @@ def energy_plot():
                 u_dt[i] += abs(potential_energy(r) + 0.5 * M * (v[0]**2+v[1]**2) - u0) * 100 / (u0 * len(rs))
         
         times = np.arange(0, len(rs), 1)*dt
-        pyplot.plot(times, u_dt, label=f'dt={dt}')
+        pyplot.plot(times[1:], u_dt[1:], label=f'dt={dt}')
     
     pyplot.xlabel('Tiempo (s)')
     pyplot.ylabel('Variacion de Et (%)')
@@ -270,69 +257,19 @@ def energy_plot():
     pyplot.legend()
     pyplot.show()
 
-def absortion_escape_plot():
-    global stop_reason, v0, r0
 
-    V0 = [5e3, 10e3, 20e3, 30e3, 35e3, 40e3, 45e3, 50e3]
-    Y0 = np.linspace(L/2-D, L/2+D, 25, endpoint=True)
-
-    samples = len(Y0)
-    p_abs, p_exited, p_left, p_right, p_top, p_bottom = [], [], [], [], [], []
-
-    for _v0 in V0:
-        absorbed, exited, left, right, top, bottom = 0, 0, 0, 0, 0, 0
-
-        for _y0 in Y0:
-            print(f'V0: {_v0} - Y0: {_y0}')
-            v0 = np.array([_v0, 0])
-            r0 = np.array([0, _y0])
-            
-            r,v = verlett(v0, r0)
-
-            if 'particle_absorbed' == stop_reason:
-                absorbed += 1
-            else:
-                exited += 1
-                if "exited_left" == stop_reason:
-                    left += 1
-                if "exited_right" == stop_reason:
-                    right += 1
-                if "exited_top" == stop_reason:
-                    top += 1
-                if "exited_bottom" == stop_reason:
-                    bottom += 1
-        
-        p_abs.append(absorbed / samples * 100)
-        p_exited.append(exited / samples * 100)
-        p_left.append(left / samples * 100)
-        p_right.append(right / samples * 100)
-        p_top.append(top / samples * 100)
-        p_bottom.append(bottom / samples * 100)
-
-    pyplot.scatter(V0, p_abs, label='Absorbidas')
-    pyplot.scatter(V0, p_exited, label='Escapadas')
-    pyplot.scatter(V0, p_left, label='Escapadas (izquierda)')
-    pyplot.scatter(V0, p_right, label='Escapadas (derecha)')
-    pyplot.scatter(V0, p_top, label='Escapadas (arriba)')
-    pyplot.scatter(V0, p_bottom, label='Escapadas (abajo)')
-
-    pyplot.legend()
-    pyplot.yscale('log')
-    pyplot.xlabel('V0 (m/s)')
-    pyplot.ylabel('Proporción (%)')
-    pyplot.show()
-
-
-def ej2_2():
-    y_values = [random.uniform(0, L) for _ in range(50)]
-    v_values = [5e3+11250*i for i in range(5)]
+def trajectory_vs_dt_plot():
+    y_values = [random.uniform(0, L) for _ in range(25)]
+    # v_values = [5e3+11250*i for i in range(5)]
+    v_values = [5e3, 10e3, 15e3, 20e3, 25e3, 30e3, 35e3, 40e3, 45e3, 50e3]
     avg_long = list()
     std = list()
 
     for v_value in v_values:
         trajectories = list()
         for y_value in y_values:
-            r, v = verlett(np.array([v_value, 0]), np.array([0, y_value]), False)
+            print(f'V0: {v_value} - Y0: {y_value}')
+            r, v = verlett(r0=[0, y_value], v0=[v_value, 0])
 
             # calculo la longitud de la trayectoria de la particula
             aux = 0
@@ -356,8 +293,109 @@ def ej2_2():
     pyplot.ylabel('Longitud de trayectoria (m)')
     pyplot.show()
 
-# energy_check()
-# animate()
-# energy_plot()
-# trajectory_plot()
-ej2_2()
+
+def absortion_escape_plot():
+    global stop_reason
+
+    V0 = [40e3, 45e3, 50e3]
+    # V0 = [5e3, 10e3, 15e3, 20e3, 25e3, 30e3, 35e3, 40e3, 45e3, 50e3]
+    Y0 = np.linspace(L/2-D, L/2+D, 25, endpoint=True)
+
+    samples = len(Y0)
+    p_abs, p_exited, p_left, p_right, p_top, p_bottom = [], [], [], [], [], []
+
+    for v0 in V0:
+        absorbed, exited, left, right, top, bottom = 0, 0, 0, 0, 0, 0
+
+        for y0 in Y0:
+            print(f'V0: {v0} - Y0: {y0}')
+
+            r,v = verlett(r0=[0, y0], v0=[v0, 0])
+
+            if 'particle_absorbed' == stop_reason:
+                absorbed += 1
+            else:
+                exited += 1
+                if "exited_left" == stop_reason:
+                    left += 1
+                if "exited_right" == stop_reason:
+                    right += 1
+                if "exited_top" == stop_reason:
+                    top += 1
+                if "exited_bottom" == stop_reason:
+                    bottom += 1
+        
+        p_abs.append(absorbed / samples * 100)
+        p_exited.append(exited / samples * 100)
+        p_left.append(left / samples * 100)
+        p_right.append(right / samples * 100)
+        p_top.append(top / samples * 100)
+        p_bottom.append(bottom / samples * 100)
+        print(absorbed)
+
+    pyplot.scatter(V0, p_abs, label='Absorbidas')
+    pyplot.scatter(V0, p_exited, label='Escapadas')
+    # pyplot.scatter(V0, p_left, label='Escapadas (izquierda)')
+    # pyplot.scatter(V0, p_right, label='Escapadas (derecha)')
+    # pyplot.scatter(V0, p_top, label='Escapadas (arriba)')
+    # pyplot.scatter(V0, p_bottom, label='Escapadas (abajo)')
+
+    pyplot.legend()
+    pyplot.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+    pyplot.xlabel('V0 (m/s)')
+    pyplot.ylabel('Proporción (%)')
+    pyplot.show()
+
+
+def trajectory_pdf_plot():
+    global stop_reason
+
+    V0 = [40e3, 45e3, 50e3]
+    Y0 = np.linspace(L/2-D, L/2+D, 100, endpoint=True)
+
+    # trajectory
+    for v0 in V0:
+        trajectory_lengths = []
+
+        for y0 in Y0:
+            print(f'V0: {v0} - Y0: {y0}')
+            
+            r,v = verlett(r0=[0, y0], v0=[v0, 0])
+
+            if stop_reason == 'particle_absorbed':
+                length = 0
+                for i in range(1, len(r)):
+                    length += math.dist([r[i-1][0], r[i-1][1]], [r[i][0], r[i][1]])
+                trajectory_lengths.append(length)
+        
+        print(trajectory_lengths)
+        pyplot.hist(trajectory_lengths, density = True, histtype="step", label=f'V0={"{:.1e}".format(v0)}')
+    
+    pyplot.legend()
+    pyplot.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+    
+    pyplot.xlabel('Longitud de la trayectoria (m)')
+    pyplot.ylabel('Densidad de probabilidad')
+
+    # pyplot.xscale('log')
+    pyplot.yscale('log')
+
+    pyplot.show()
+
+
+# ---------------------------------------------------------------
+#                         EJECUCIÓN
+# ---------------------------------------------------------------
+
+# Parámetros de simulación
+dt = 1e-13
+log_step = 10
+# tf = np.Infinity
+tf = 2e-12
+
+if __name__ == '__main__':
+    energy_plot(v0=[5e4, 0])
+
+    # animate(r0=[0, L/3], v0=[5e3, 0])
+
+    # trajectory_pdf_plot()
